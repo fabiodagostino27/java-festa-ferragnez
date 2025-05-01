46749b2b3fa69e378279ecc67df861e33a273f23
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-01 16:21:43 +0200

    ADD: Implement guest name input functionality in CheckGuest class

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,14 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
         String[] guestsList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        
+        System.out.print("Qual è il tuo nome? ");
+        Scanner in = new Scanner(System.in);
+        String guestName = in.nextLine();
+        in.close();
     }
 }
