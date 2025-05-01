568ab78d2051f27ada0651cd9aff5920c84444a1
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-01 16:36:39 +0200

    ADD: Implement guest validation and welcome message in CheckGuest class

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -5,10 +5,25 @@ import java.util.Scanner;
 public class CheckGuest {
     public static void main(String[] args) {
         String[] guestsList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
-        
+
         System.out.print("Qual è il tuo nome? ");
         Scanner in = new Scanner(System.in);
         String guestName = in.nextLine();
         in.close();
+
+        boolean isGuestFound = false;
+
+        for (int i = 0; i < guestsList.length; i++) {
+            if (guestsList[i].toLowerCase().equals(guestName.trim().toLowerCase().replaceAll("\s+", " "))) {
+                isGuestFound = true;
+                break;
+            }
+        }
+
+        if (isGuestFound) {
+            System.out.println("Benvenuto/a alla festa!");
+        } else {
+            System.out.println("Non sei presente nella lista!");
+        }
     }
 }
